4558fe8f6888d5512515064eefb21b6da281ba51
Fred <delon.frederic.pro.job@gmail.com>
2017-09-17 23:22:44 +0200

    initial +  menu

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});Route::get('/', function () {
-    return view('menu');
-});
+Route::get('/','welcomeController@welcome');
+
