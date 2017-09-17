422b9b0d876e96f696a7d5e65c86ed8a46ad57a1
Fred <delon.frederic.pro.job@gmail.com>
2017-09-17 22:55:22 +0200

    initial +  menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,23 +5,18 @@
 
 
 <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
-                 <span class="icon-bar"></span>
-                 <span class="icon-bar"></span>
-                 <span class="icon-bar"></span>
-             </button>
-         </div>
-         <div class="collapse navbar-collapse" id="menu">
-            <ul class="nav navbar-nav">
-            	<li><a href="#">LOGO</a></li>
-                <li><a href="#">NEXUS COMMUNITY</a></li>
+
+  <div class="container">
+     <ul class="nav navbar-nav navbar-right">
+     <li><img src="logoBoot.png" width="50px" height="50px" alt="logo"></li>
+<li><h3 class="titre">Nexus community</h3></li>
                 <li><a href="#">Presentation</a></li>
                 <li><a href="#">games</a></li>
                 <li><a href="#">nous rejoindre</a></li>
                 
             </ul>
-        </div>
+  </div>
+</nav>
+        
 
 @stop
