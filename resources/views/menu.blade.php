6b6ec78b7b7662d852cddf6f21cc82b7bce149c8
Fred <delon.frederic.pro.job@gmail.com>
2017-09-17 22:14:47 +0200

    initial +  menu

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,27 @@
+@extends('welcome')
+
+@section('content')
+
+
+
+<nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+             </button>
+         </div>
+         <div class="collapse navbar-collapse" id="menu">
+            <ul class="nav navbar-nav">
+            	<li><a href="#">LOGO</a></li>
+                <li><a href="#">NEXUS COMMUNITY</a></li>
+                <li><a href="#">Presentation</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">nous rejoindre</a></li>
+                
+            </ul>
+        </div>
+
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <title>Nexus</title>
 
@@ -11,15 +12,11 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 </head>
 <body>
-    
 
-    <div class="content">
-        <div class="title m-b-md">
-            NEXUS
-        </div>
 
-        
-    </div>
-</div>
+@yield('content')
+
+
+  
 </body>
 </html>
