8cb076a7080ebb4dfe9acd124a1ed2c0d895cfc6
Fred <delon.frederic.pro.job@gmail.com>
2017-09-19 22:51:49 +0200

    test btn

--- public/js/app.js
+++ public/js/app.js
@@ -90,12 +90,14 @@ $(document).ready(function () {
 	$('#btn1').click(function () {
 
 		$('body').css('background-color', '#2f2f2f');
-		$('#colorMenu').css('background-color', '#2f2f2f');
+		$('#colorMenu').css('background-color', 'white');
+		$('a').css('color', '#2f2f2f');
 	});
 	$('#btn2').click(function () {
 
 		$('body').css('background-color', 'white');
-		$('#colorMenu').css('background-color', 'white');
+		$('#colorMenu').css('background-color', '#2f2f2f');
+		$('a').css('color', 'white');
 	});
 });
 
