af70069dd1e45729fcd89042972612827b472202
Fred <delon.frederic.pro.job@gmail.com>
2017-09-19 22:38:15 +0200

    test btn

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,11 +11,14 @@ $(document).ready(function () {
 		});
 
 	$('#btn1').click(function () {
-		console.log('prout');
+		
 		$('body').css('background-color','#2f2f2f');
+		$('#colorMenu').css('background-color','#2f2f2f');
+
 	});
 	$('#btn2').click(function () {
-		console.log('prout');
+		
 		$('body').css('background-color','white');
+		$('#colorMenu').css('background-color','white');
 	});
 });
